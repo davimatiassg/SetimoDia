597ae39f7d733600150256f00bd2534c14599c75
davimatiassg <davimatiassg@gmail.com>
2023-12-29 03:54:39 -0300

    Card asset creation setup progress

---
+++ Assets/Cards/FX1.cs
@@ -0,0 +1,4 @@
+public class FX1 : CardFX
+{
+	public override void execute (CardEventCircumstances circumstances) { return; }
+}
--- Assets/Scripts/Card System/CardData.cs
+++ Assets/Scripts/Card System/CardData.cs
@@ -13,11 +13,27 @@ using UnityEditor;
 [CreateAssetMenu(fileName = "Blank Card", menuName = "Card/Create new Card", order = 1)]
 public class CardData : ScriptableObject
 {
-    [SerializeField] private Texture mainTexture { get {return mainTexture;} set {mainTexture = value;} }
-    [SerializeField] private string description { get {return description;} set {description = value;} }
-    [SerializeField] private int impact { get {return impact;} set {impact = value;} }
 
-    private Dictionary<string, UnityAction<CardEventCircumstances>> effects;
+    private struct cardEffectInfo
+    {
+        public string trigger {get; set;}
+        public List<CardFX> effects { get; set; }
+
+        public cardEffectInfo(string t, List<CardFX> fx){ trigger = t; effects = fx;}
+    }
+
+    [SerializeField] private Texture mainTexture;
+    [SerializeField] private Texture backTexture;
+    [SerializeField] private string description;
+    [SerializeField] private string cardName;
+    [SerializeField] private int impact;
+
+    private List<cardEffectInfo> effects;
+
+    public CardData()
+    {
+        effects = new List<cardEffectInfo>();
+    }
 
     #region Editor
     #if UNITY_EDITOR
@@ -27,22 +43,73 @@ public class CardData : ScriptableObject
     {
         public override void OnInspectorGUI()
         {
-            base.OnInspectorGUI();
-
             CardData data = (CardData)target;
-            
-            EditorGUILayout.BeginHorizontal();
 
-            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.PrefixLabel("Card Name");
+            data.cardName = (string)EditorGUILayout.TextField(data.cardName);
 
-            foreach (var item in data.effects)
+            EditorGUILayout.PrefixLabel("Card Description");
+            data.description = (string)EditorGUILayout.TextArea(data.description, GUILayout.MinHeight(60));
+
+            EditorGUILayout.PrefixLabel("Main Texture");
+            data.mainTexture = (Texture)EditorGUILayout.ObjectField(data.mainTexture, typeof(Texture), false);
+
+            EditorGUILayout.PrefixLabel("Background Texture");
+            data.backTexture = (Texture)EditorGUILayout.ObjectField(data.backTexture, typeof(Texture), false);
+
+            EditorGUILayout.PrefixLabel("Card Impact");
+            data.impact = (int)EditorGUILayout.IntField(data.impact);
+
+            bool CardFXGroup = false;
+            int size = data.effects.Count;
+            CardFXGroup = EditorGUILayout.BeginFoldoutHeaderGroup(CardFXGroup, "Card Effects");
+            if(true)
             {
-                EditorGUILayout.Space();
+                
+                size = (int)EditorGUILayout.IntField(size);
 
+                while(size > data.effects.Count)
+                {
+                    data.effects.Add(new cardEffectInfo("", new List<CardFX>()));
+                }
+
+                while(size < data.effects.Count)
+                {
+                    data.effects.RemoveAt(data.effects.Count-1);
+                }
+
+                for(int i = 0; i < data.effects.Count; i++)
+                {
+                    cardEffectInfo item = data.effects[i];
+                    EditorGUILayout.PrefixLabel(String.Format("Element {0}", i));
+                    EditorGUILayout.BeginHorizontal();
+                    EditorGUILayout.PrefixLabel("Event");
+                    item.trigger = (string)EditorGUILayout.TextArea(item.trigger, GUILayout.MinHeight(60));
+                    EditorGUILayout.EndHorizontal();
+
+                    EditorGUILayout.BeginHorizontal();
+                    EditorGUILayout.PrefixLabel("Effect");
+                    EditorGUILayout.BeginVertical();
+                    for(int j = 0; j < item.effects.Count; j++)
+                    {
+                        item.effects[j] = (CardFX)EditorGUILayout.ObjectField(item.effects[j], typeof(CardFX), false);
+                    }
+                    CardFX newFX = (CardFX)EditorGUILayout.ObjectField(null, typeof(CardFX), false);
+                    if(newFX != null)
+                    {
+                        item.effects.Add(newFX);
+                    }
+                    EditorGUILayout.EndVertical();
+                    EditorGUILayout.EndHorizontal();
+                    EditorGUILayout.Space();
+                }
+
+                //data.ApplyModifiedProperties();
             }
-            EditorGUILayout.Space();
+            EditorGUILayout.EndFoldoutHeaderGroup();
 
         }
+
     }
 
     #endif    
